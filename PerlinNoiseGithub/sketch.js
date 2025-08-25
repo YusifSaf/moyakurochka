516be273adb7f105b3663991db8f2c7bcc2a285e
YusifSaf <yusif.228007@hotmail.com>
2025-08-25 11:53:55 +0400

    Cleaned up the sketch comments
    YAY! :>

--- PerlinNoiseGithub/sketch.js
+++ PerlinNoiseGithub/sketch.js
@@ -57,21 +57,11 @@ function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     rectMode(CENTER);
     angleMode(DEGREES);
-    // colorMode(HSB);
 
     cam = createCamera();
     cam.move(0, -50, -50);
-
     tint(255, 0); 
-    // camera(0, 0, 250);
 
-    // Start the mic
-    // mic = new p5.AudioIn();
-    // mic.start();
-    
-    //Playing the song and setting up FFT
-    // song.play();
-    // song.stop();
     fft = new p5.FFT(smoothing, bins);
 }
 
@@ -94,6 +84,7 @@ function cubeGrid(amp) {
       let r = map(noise(zOff), 0, 1, 50, 255);   // cut off dark grays
       let g = map(noise(yOff), 0, 1, 50, 255);
       let b = map(noise(xOff + 15), 0, 1, 50, 255);
+      fill(r, g, b);
 
       // Another option
       // colorMode(HSB, 255);
@@ -101,19 +92,13 @@ function cubeGrid(amp) {
       // let s = map(noise(yOff, zOff), 0, 1, 180, 200);   // keep saturation high
       // let b = map(noise(xOff, zOff), 0, 1, 180, 200);   // keep brightness high
       // fill(h, s, b);
-      fill(r, g, b);
-      // fill(50);    
-      // rect(size/2 + i*size, size/2 + j*size, sizes[i][j], sizes[i][j]);
+
       //Drawing multiple boxes
       push();
       translate(size * i - 100, sizes[i][j] * map(amp, 0, 255, 0.25, 15) * 3, size * j - 100);
       box(size, 300, size);
       pop();
     }
-    // rotateY(map(amp, 0, 255, 0, 180));
-    // rotateZ(map(amp, 0, 255, 0, 180));
-    // rotateX((map(noise(zOff), 0, 1, 0, 45)))
-    // rotateY(map(noise(zOff), 0, 1, 0, 45));
     yOff = 0;
   }
   zOff += 0.003;
@@ -124,12 +109,6 @@ function imageGrid(){
   let rectImgIndex = 0;
 
   for (let i=0; i<360; i+=30){
-    // xOff += 0.03
-    // yOff += 0.03
-    // let n = noise(xOff, yOff, zOff);
-    // let nMove = map(n, 0, 1, 0, 360);
-    
-
     push();
     rotateX(i);
     // line(0, 0, 0, 0, 0, 200);
@@ -137,7 +116,6 @@ function imageGrid(){
 
     push();
     rotateX(i);
-    // rotateY(i + nMove) //for organic movement with noise
     translate(0, tDistance, 0);
     rotateX(atan2(1, 0))
     texture(rImage[rectImgIndex]);
@@ -175,6 +153,7 @@ function wasdMove(){
       // S - backward, W - forward
       (keyIsDown(83) ? speed : 0) + (keyIsDown(87) ? -speed : 0)
     );
+    //This was taken from this great gentleman https://stackoverflow.com/questions/67817980/how-can-i-create-a-3d-game-in-p5-js-camera-positions?utm_source=chatgpt.com
 }
 
 function songAnalyze() {
@@ -200,23 +179,11 @@ function playSong(songNum){
         if(fft){
             fft.setInput(currentSong);
         }
-
-        //Update UI here
     }
 
     else{
         console.log("song not loaded yet");
     }
-
-    //Stop all the songs
-    // songs.forEach((song, i) => {
-    //     if(i != songNum){
-    //         song.stop();
-    //     }
-    // });
-
-    //Play the desired song
-    // songs[songNum].play();
 }
 
 function tooglePlayPause(){
@@ -243,11 +210,6 @@ function updateProgress(value){
 }
 
 function updateProgressUI(){
-    // for(i=0; i<currentSong.duration(); i++){
-    //     let moment = map(i, 0, currentSong.duration(), 0, 100)
-    //     progressBar.value = moment
-    //     print("moment:", moment);
-    // }
     if (currentSong && currentSong.isPlaying()){
         let minute = map(currentSong.currentTime(), 0, currentSong.duration(), 0, 100)
         progressBar.value = minute;
@@ -255,19 +217,11 @@ function updateProgressUI(){
 }
 
 function hideUI(){
-  // let elems = document.getElementsByClassName("keyboard-controls");
-  // for (let i; i<elems.length; i++){
-  //   elems[i].style.opacity = "0";
-  // }
   document.getElementById("controls").style.opacity = "0";
   document.getElementById("coming-soon").style.opacity = "0";
 }
 
 function showUI(){
-  // let elems = document.getElementsByClassName("keyboard-controls");
-  // for (let i; i<elems.length; i++){
-  //   elems[i].style.opacity = "0";
-  // }
   document.getElementById("controls").style.opacity = "1";
   document.getElementById("coming-soon").style.opacity = "1";
 }
@@ -281,18 +235,19 @@ function Lighting() {
     pointLight(50, 25, 100, -200, -100, 200);
 }
 
-//U STOPPED HERE. Need to make play/stop, and scroll functionality. Then in css add some sweet text on the right
 function draw() {
   background(46, 52, 66);
   noStroke();
   orbitControl();
-
   wasdMove();
+
   
   userStartAudio(); // Ensure audio context is started
   let amp = songAnalyze();
+  
   updateProgressUI();
 
+  //Show UI and Images on music play
   if(amp>0){
     imageGrid();
     hideUI();
@@ -300,44 +255,14 @@ function draw() {
   if(amp==0){
     showUI();
   }
-  // Lighting();
-  cubeGrid(amp);
-
-    // ADD LIGHTING HERE
-    // Lighting();
-
-    //Getting mic volume
-    // amp = mic.getLevel() * 100
-    // amplitudeCoeff = map(amp, 0, 100, 0, 10)
-
-    //Analyzing the song. Use .getEnergy() for specific frequencies
-    // like this amp = fft.getEnergy("bass");
-    
-    // print("spectrum:" + spectrum);
-    // print("amp:" + amp);
-    // print("amplitudeCoeff:" + amplitudeCoeff);
-    // Adjust the range as needed
-    
-
-    //Using .waveform() or .analyze()
-    // waveform = fft.waveform();
-    // print(waveform);
-
-    //Drawing the grid
-   
+  
+  // Lighting(); to enable lighting
+  cubeGrid(amp); //Drawing the grid
 }
 
-
 // To do:
-// Functions:
-
-// - Images appear when music is Playing (?)
-// - Small text box with controls (+)
-// - Music controls opacity (+)
-// - Random Picture choice in ChatGPT
-
-
 // 1. Tempo is the inc amount for the zOff parameter
 // 2. Volume as the box height or amplitudeCoeff (+)
 // 3. Camera go down with the box and come closer when it goes down
+
 
